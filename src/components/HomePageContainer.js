4c92b8a52ceaef1f9e2f3bead723f7f7cc8c403f
learnerdilip <diliptalks@gmail.com>
2020-03-31 17:31:05 +0200

    changes to the google signin

--- src/components/HomePageContainer.js
+++ src/components/HomePageContainer.js
@@ -3,18 +3,12 @@ import RoomContainer from "./room/RoomContainer";
 import { Button } from "react-bootstrap";
 
 const HomePageContainer = () => {
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+  
 
   return (
     <div className="homepage">
       <h2>Manage your Warranty and Invoices</h2>
-      <Button onClick={onSignIn}>GET GOOGLE DATA</Button>
+      {/* <Button onClick={onSignIn}>GET GOOGLE DATA</Button> */}
       <RoomContainer />
     </div>
   );
